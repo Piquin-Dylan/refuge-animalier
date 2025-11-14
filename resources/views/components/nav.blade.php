11d3d6dce9ae144d3cf3650d13b566353a0b4042
Piquin-Dylan <dylan.piquin@student.hepl.be>
2025-11-14 16:23:21 +0100

    Creation file nav.blade + creation of the nav

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <h1 class="hidden">Page d'accueil</h1>
+
+    <nav>
+        <h2>Navigation principal</h2>
+
+        <a href="">Accueil</a>
+        <a href="">A propos</a>
+        <a href="">Adopter</a>
+        <a href="">Contact</a>
+    </nav>
+
+</div>
