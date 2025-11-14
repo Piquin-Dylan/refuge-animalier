3fc987de9d9226a6e6ade3e28f2ae218b6b0fd69
Piquin-Dylan <dylan.piquin@student.hepl.be>
2025-11-14 16:47:23 +0100

    creation of the nav component + implementation

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,13 +1,10 @@
 <div>
     <h1 class="hidden">Page d'accueil</h1>
-
     <nav>
         <h2>Navigation principal</h2>
-
         <a href="">Accueil</a>
         <a href="">A propos</a>
         <a href="">Adopter</a>
         <a href="">Contact</a>
     </nav>
-
 </div>
