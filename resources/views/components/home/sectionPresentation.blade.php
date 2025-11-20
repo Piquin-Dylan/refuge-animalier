4e5725c426793b54d8f0ef96ff9cb2b35711d648
Piquin-Dylan <dylan.piquin@student.hepl.be>
2025-11-20 11:11:15 +0100

    refactoring

--- resources/views/components/home/sectionPresentation.blade.php
+++ resources/views/components/home/sectionPresentation.blade.php
@@ -1,25 +1,7 @@
-
-@props([
-    'title_section',
-    'title_content',
-    'content',
-    'cta1' => null,
-    'cta2'=> null,
-    'image'=>null
-])
-
-
-<section class="pb-6  pl-5 pr-5 sectionFlex ">
-    <div class="max-w-content">
-        {{$slot}}
-        <h2 class="hidden">{{$title_section}}</h2>
-        <p class="font-bold pb-4 text-title">{{$title_content}}</p>
-        <p class="text-subtitle">{{$content}}</p>
-        <div class="flex gap-8 justify-center pt-5 pb-5">
-            <x-button class="cta1">{{$cta1}}</x-button>
-            <x-button class="cta2">{{$cta2}}</x-button>
-        </div>
-    </div>
-    <img class=" lg:max-w-image lg:max-h-image" alt="Image représentant une personne avec un animal"
-         src="{{$image}}">
-</section>
+<x-section
+    title_section="section présentation"
+    title_content="{{__('home.TitleSectionPresentation')}}"
+    content="{{__('home.MainContentSectionPresentation')}}"
+    cta1="{{__('home.LearnMore')}}" cta2="{{__('home.Adoption')}}"
+    image="{{asset('ImageSectionPrésentation.png')}}">
+</x-section>
